55997cb132b8eafe2ba7226dcd30191dab679fbb
Atchariya <atchariya.nna@gmail.com>
2025-10-07 12:38:24 +0000

    add serverlesshttp libraly

--- server/api/index.js
+++ server/api/index.js
@@ -1,20 +1,13 @@
 import express from "express";
-import profileRouter from "../routes/profile.js";
 import serverless from "serverless-http";
+import profileRouter from "../routes/profile.js";
 
 const app = express();
-
 app.use(express.json());
 app.use("/api", profileRouter);
 
-app.get("/", async (req, res) => {
-    //1. access body and req
-    //2. sql statmen
-    //3. res section
-    return res.status(200).json({
-        "success": true,
-        "message": "test pass"
-    })
-})
+app.get("/", (req, res) => {
+  res.json({ success: true, message: "test pass" });
+});
 
 export const handler = serverless(app);
